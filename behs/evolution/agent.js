694ed63edcfd503530a0c2b27e1014bdbab74300
Prakhar Mehrotra <Prakhar Mehrotra>
2017-08-05 16:55:31 -0400

    Agent Update

--- behs/evolution/agent.js
+++ behs/evolution/agent.js
@@ -1,39 +1,60 @@
+import * as Display from 'display'
+import P5Behavior from 'p5beh';
+import * as Floor from 'floor';
+import World from './evolution/world';
+import User from './user';
+import DNA from './dna';
+var Vec2D = require('vector2d');
+
+
 class Agent {
-  //dns: new DNA()
+  constructor(position,dna){
+  	this.dna = dna
+  	this.state = {
+  		position:Vec2D.ObjectVector(position.x,position.y),
+  		velocity:Vec2D.ObjectVector(0,0),
+  		acceleration:Vec2D.ObjectVector(0,0),
+  		alive: true,
+  		health:
+  	}
+  }
+
+  update(agents){
+
+  }
 }
 
-<<<<<<< HEAD
 module.exports = Agent;
-
 
 
 export default Agent;
 
-Agent Architecture
 
-Init(DNA, Position)
+// Agent Architecture
 
-state[pos, vel, accel, alive, health, ]
-DNA [lifespan, replicationProb, maxSpeed, maxAccel, deathRepel, vision, attractOthers, repelPlayer, movementVariability, mutationRate]
+// Init(DNA, Position)
+
+// state[position, velocity, acceleration, alive, health]
+// DNA [lifespan, replicationProb, maxSpeed, maxAccel, deathRepel, vision, attractOthers, repelPlayer, movementVariability, mutationRate]
 
 
 
-Functions
+// Functions
 
-update (list of agents)
-	add up accel (limit) -> vel (limit)-> pos
-	accel * 0
-	limitLocation	
-	display
+// update (list of agents)
+// 	add up accel (limit) -> vel (limit)-> pos
+// 	accel * 0
+// 	limitLocation	
+// 	display
 
-lifespan
-	counts down from lifespan 
+// lifespan
+// 	counts down from lifespan 
 
-limitLocation
-	agent wraps around borders
+// limitLocation
+// 	agent wraps around borders
 
-Attract Others
+// Attract Others
 
-for a in agents
-	if a distance is less than vision
-		decide if 
+// for a in agents
+// 	if a distance is less than vision
+// 		decide if 

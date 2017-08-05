798caa03121d3ce7f4bd252df447c279b1a6b31b
Ryan Villanueva <ryanvill@gmail.com>
2017-08-05 16:49:24 -0400

    Merge

--- behs/evolution/agent.js
+++ behs/evolution/agent.js
@@ -13,38 +13,4 @@ class Agent {
   }
 }
 
-<<<<<<< HEAD
-module.exports = Agent;
-
-
-
 export default Agent;
-
-Agent Architecture
-
-Init(DNA, Position)
-
-state[pos, vel, accel, alive, health, ]
-DNA [lifespan, replicationProb, maxSpeed, maxAccel, deathRepel, vision, attractOthers, repelPlayer, movementVariability, mutationRate]
-
-
-
-Functions
-
-update (list of agents)
-	add up accel (limit) -> vel (limit)-> pos
-	accel * 0
-	limitLocation	
-	display
-
-lifespan
-	counts down from lifespan 
-
-limitLocation
-	agent wraps around borders
-
-Attract Others
-
-for a in agents
-	if a distance is less than vision
-		decide if 
